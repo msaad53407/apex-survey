60f39e6e8a778415168171680af2ad574908a5d3
Muhammad Saad <msmuhammadsaad78@gmail.com>
2025-04-22 17:18:45 +0500

    Refactor next.config.mjs for improved readability and consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	compiler: {
+		removeConsole: {
+			exclude: ["error", "warn"],
+		},
+	},
 };
 
 export default nextConfig;
