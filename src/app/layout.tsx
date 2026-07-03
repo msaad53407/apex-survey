6e796213c4e4a11165706ed78c2c3abc34d74894
Muhammad Saad <msmuhammadsaad78@gmail.com>
2026-07-03 15:08:08 +0500

    Enhance RootLayout component by adding Google Tag Manager scripts, improving code formatting, and ensuring consistent import order.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,62 @@
+import { ProjectProvider } from "@/context/ActiveProjectContext";
+import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Lato, Montserrat } from "next/font/google";
-import { cn } from "@/lib/utils";
+import Script from "next/script";
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import toast, { Toaster } from "react-hot-toast";
-import { ProjectProvider } from "@/context/ActiveProjectContext";
 
-const lato = Lato({ 
-	subsets: ["latin"], 
-	weight: ["100", "300", "400", "700", "900"],
-	variable: "--font-lato" 
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  variable: "--font-lato",
 });
 
-const montserrat = Montserrat({ 
-	subsets: ["latin"], 
-	variable: "--font-montserrat" 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
-	title: "Fairdeal Electrical",
-	description:
-		"Fairdeal Electrical offers top-notch electrical services including installations, rewiring, testing, fuse board repair, lighting upgrades, and smoke alarm installations. Our certified electricians ensure safety, efficiency, and compliance with all regulations for residential, commercial, and industrial properties. Contact us for reliable and professional electrical solutions.",
+  title: "Fairdeal Electrical",
+  description:
+    "Fairdeal Electrical offers top-notch electrical services including installations, rewiring, testing, fuse board repair, lighting upgrades, and smoke alarm installations. Our certified electricians ensure safety, efficiency, and compliance with all regulations for residential, commercial, and industrial properties. Contact us for reliable and professional electrical solutions.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={cn(
-					"bg-background font-sans antialiased min-w-full",
-					lato.variable,
-					montserrat.variable
-				)}>
-				<ProjectProvider>
-					<main className="min-h-screen">
-						<Toaster />
-						{children}
-					</main>
-				</ProjectProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "bg-background font-sans antialiased min-w-full",
+          lato.variable,
+          montserrat.variable,
+        )}
+      >
+        <ProjectProvider>
+          <main className="min-h-screen">
+            <Toaster />
+            {children}
+          </main>
+        </ProjectProvider>
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HZW5NJW5RX"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments)}
+			gtag('js', new Date());
+			gtag('config', 'G-HZW5NJW5RX');
+  		`}
+        </Script>
+      </body>
+    </html>
+  );
 }
